735d8f45e480ed2d82d4d5d8d1401f7baa188565
siteshbade <49814332+siteshbade@users.noreply.github.com>
2021-06-03 12:28:06 +0530

    update password for root user

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,4 +1,4 @@
 <?php 
-	mysql_connect("localhost","root","") or die("Couldn't connet to SQL server");
+	mysql_connect("localhost","root","root1234") or die("Couldn't connet to SQL server");
 	mysql_select_db("ebuybd") or die("Couldn'ttt select DB");
 ?>
